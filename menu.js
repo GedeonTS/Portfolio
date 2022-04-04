48dcebc82a440a15003fa47358e7cab6801c9015
GedeonTS <gedeontshobohwa1@gmail.com>
2022-04-04 21:27:42 +0200

    add scripts to the mobile menu

--- menu.js
+++ menu.js
@@ -1,20 +1,20 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
-const canceli = document.getElementById("canceli");
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const canceli = document.getElementById('canceli');
 
-open.addEventListener("click", () => {
-  open.classList.add("hide");
-  open.classList.remove("show");
-  close.classList.remove("hide");
-  close.classList.add("show");
-  canceli.classList.remove("hide");
-  canceli.classList.add("show");
+open.addEventListener('click', () => {
+  open.classList.add('hide');
+  open.classList.remove('show');
+  close.classList.remove('hide');
+  close.classList.add('show');
+  canceli.classList.remove('hide');
+  canceli.classList.add('show');
 });
-canceli.addEventListener("click", () => {
-  close.classList.add("hide");
-  close.classList.remove("show");
-  canceli.classList.add("hide");
-  canceli.classList.remove("show");
-  open.classList.remove("hide");
-  open.classList.add("show");
+canceli.addEventListener('click', () => {
+  close.classList.add('hide');
+  close.classList.remove('show');
+  canceli.classList.add('hide');
+  canceli.classList.remove('show');
+  open.classList.remove('hide');
+  open.classList.add('show');
 });
