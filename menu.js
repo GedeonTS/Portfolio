620246249cab6a74141640b305c2ac032966cde3
GedeonTS <gedeontshobohwa1@gmail.com>
2022-04-04 20:58:32 +0200

    set js event on hamburger button

--- menu.js
+++ menu.js
@@ -4,16 +4,18 @@ const canceli = document.getElementById("canceli");
 
 open.addEventListener("click", () => {
   open.classList.add("hide");
+  open.classList.remove("show");
   close.classList.remove("hide");
   close.classList.add("show");
   canceli.classList.remove("hide");
-  canceli.classList.add("show");
+    canceli.classList.add("show");
+    close.focus();
 });
-
 canceli.addEventListener("click", () => {
   close.classList.add("hide");
   close.classList.remove("show");
   canceli.classList.add("hide");
+  canceli.classList.remove("show");
   open.classList.remove("hide");
   open.classList.add("show");
 });
