a92f8d8331397cd0019adde7a38d1f078666fe5c
GedeonTS <gedeontshobohwa1@gmail.com>
2022-04-05 12:11:10 +0200

    add srcipt to the mobile menu

--- menu.js
+++ menu.js
@@ -1,50 +1,50 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
-const canceli = document.getElementById("canceli");
-const link1 = document.getElementById("elem1");
-const link2 = document.getElementById("elem2");
-const link3 = document.getElementById("elem3");
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const canceli = document.getElementById('canceli');
+const link1 = document.getElementById('elem1');
+const link2 = document.getElementById('elem2');
+const link3 = document.getElementById('elem3');
 
-open.addEventListener("click", () => {
-  open.classList.add("hide");
-  open.classList.remove("show");
-  close.classList.remove("hide");
-  close.classList.add("show");
-  canceli.classList.remove("hide");
-  canceli.classList.add("show");
+open.addEventListener('click', () => {
+  open.classList.add('hide');
+  open.classList.remove('show');
+  close.classList.remove('hide');
+  close.classList.add('show');
+  canceli.classList.remove('hide');
+  canceli.classList.add('show');
 });
-canceli.addEventListener("click", () => {
-  close.classList.add("hide");
-  close.classList.remove("show");
-  canceli.classList.add("hide");
-  canceli.classList.remove("show");
-  open.classList.remove("hide");
+canceli.addEventListener('click', () => {
+  close.classList.add('hide');
+  close.classList.remove('show');
+  canceli.classList.add('hide');
+  canceli.classList.remove('show');
+  open.classList.remove('hide');
+  open.classList.add('show');
+});
+
+link1.addEventListener('click', () => {
+  close.classList.add('hide');
+  close.classList.remove('show');
+  canceli.classList.add('hide');
+  canceli.classList.remove('show');
+  open.classList.remove('hide');
   open.classList.add("show");
 });
 
-link1.addEventListener("click", () => {
-  close.classList.add("hide");
-  close.classList.remove("show");
-  canceli.classList.add("hide");
-  canceli.classList.remove("show");
-  open.classList.remove("hide");
-  open.classList.add("show");
+link2.addEventListener('click', () => {
+  close.classList.add('hide');
+  close.classList.remove('show');
+  canceli.classList.add('hide');
+  canceli.classList.remove('show');
+  open.classList.remove('hide');
+  open.classList.add('show');
 });
 
-link2.addEventListener("click", () => {
-  close.classList.add("hide");
-  close.classList.remove("show");
-  canceli.classList.add("hide");
-  canceli.classList.remove("show");
-  open.classList.remove("hide");
-  open.classList.add("show");
-});
-
-link3.addEventListener("click", () => {
-  close.classList.add("hide");
-  close.classList.remove("show");
-  canceli.classList.add("hide");
-  canceli.classList.remove("show");
-  open.classList.remove("hide");
-  open.classList.add("show");
+link3.addEventListener('click', () => {
+  close.classList.add('hide');
+  close.classList.remove('show');
+  canceli.classList.add('hide');
+  canceli.classList.remove('show');
+  open.classList.remove('hide');
+  open.classList.add('show');
 });
