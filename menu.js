e851126f4ea8772acc07a9ea1057e5498606aa8f
jaujau088 <jaujau088@gmail.com>
2022-04-08 16:47:38 +0200

    Add form local storage js

--- menu.js
+++ menu.js
@@ -419,3 +419,39 @@ form.onsubmit = (e) => {
     form.onsubmit = true;
   }
 };
+
+// Form local storage availability checker function
+function isStorageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      // 1) everything except Firefox
+      // 2) Firefox
+      // test name field too, because code might not be present
+      // 3) everything except Firefox
+      // 4) Firefox
+      // 5) acknowledge QuotaExceededError only if there's something already stored
+      e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && (storage && storage.length !== 0);
+  }
+}
+
+// Form local storage:
+// When the user changes the content of any input field, the data is saved to the local storage
+form.addEventListener('input', () => {
+  // Check if there is local storage
+  if (isStorageAvailable('localStorage')) {
+    // Update the storage values
+    formStorage.name = formName.value;
+    formStorage.email = formEmail.value;
+    formStorage.message = formMsg.value;
+
+    // Store them to the localStorage
+    localStorage.setItem('formValues', JSON.stringify(formStorage));
+  }
+});
