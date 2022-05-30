6852d4c747f775e8effd0f16739b1ed1e9396bfc
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-30 14:12:24 +0200

    adjut gap to the works section

--- scripts.js
+++ scripts.js
@@ -44,39 +44,27 @@ popupLinks.forEach(link => {
 });
 
 
-// work section
-const projects = [{ imageMobile: '', imageDesktop: '', projectTitle: '', stack:'',year:''}]
-const workSection = document.querySelector('.works')
-workSection.innerHTML = `<article>
-<img src="./IMG/SnapshootPortfolio.png" class="card-image-mobile">
-<img src="./IMG/SnapshootPortfolio.png" class="card-image-desktop">
+// works section
+const projects = [{ imageMobile: './IMG/SnapshootPortfolio.png', imageDesktop: './IMG/SnapshootPortfolio.png', projectTitle: 'Leaderboard', stack: 'Front End Dev', year: '2022', order: '', description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.', technologies: ["javasCript", "Html", "css"] },{ imageMobile: './IMG/SnapshootPortfolio.png', imageDesktop: './IMG/SnapshootPortfolio.png', projectTitle: 'Leaderboard', stack: 'Front End Dev', year: '2022', order: '', description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.', technologies: ["javasCript", "Html", "css"] },{ imageMobile: './IMG/SnapshootPortfolio.png', imageDesktop: './IMG/SnapshootPortfolio.png', projectTitle: 'Leaderboard', stack: 'Front End Dev', year: '2022', order: '', description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.', technologies: ["javasCript", "Html", "css"] }]
+const workSection = document.querySelector('.works');
+projects.forEach(project => {
+    workSection.innerHTML += `<article>
+<img src="${project.imageMobile}" class="card-image-mobile">
+<img src=".${project.imageDesktop}" class="card-image-desktop">
 <section class="side-section">
-<h2 class="project-title">Tonic</h2>
+<h2 class="project-title">${project.projectTitle}</h2>
 <ul class="project-highlights">
 <li class="organisation">Microverse</li>
 <li><img class="counter" src="./ICONS/Counter.png"></li>
-<li>Front End Dev</li>
+<li>${project.stack}</li>
 <li><img class="counter" src="./ICONS/Counter.png"></li>
-<li>2022</li>
+<li>${project.year}</li>
 </ul>
-<p class="project-descritpion">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
-<ul class="stacks"><li class="stack">javaScript</li><li class="stack">Html</li></ul>
-<button class="project-btn">See project</button>
-</section>
-</article><article>
-<img src="./IMG/SnapshootPortfolio.png" class="card-image-mobile">
-<img src="./IMG/SnapshootPortfolio.png" class="card-image-desktop">
-<section class="side-section">
-<h2 class="project-title">Tonic</h2>
-<ul class="project-highlights">
-<li class="organisation">Microverse</li>
-<li><img class="counter" src="./ICONS/Counter.png"></li>
-<li>Front End Dev</li>
-<li><img class="counter" src="./ICONS/Counter.png"></li>
-<li>2022</li>
-</ul>
-<p class="project-descritpion">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
-<ul class="stacks"><li class="stack">javaScript</li><li class="stack">Html</li></ul>
+<p class="project-descritpion">${project.description}</p>
+<ul class="stacks"><li class="stack">${project.technologies[0]}</li><li class="stack">${project.technologies[1]}</li><li class="stack">${project.technologies[2]}</li></ul>
 <button class="project-btn">See project</button>
 </section>
 </article>`
+
+
+})
